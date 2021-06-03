528a0bd5fadfd17cf26c907ea8f31c72a920ec47
Ikem Nomso <ikemnomso@gmail.com>
2021-06-03 14:05:16 +0100

    nav dropdown

---
+++ index.js
@@ -0,0 +1,16 @@
+function dropDown(str) {
+	document.getElementById(str).classList.toggle('show');
+}
+
+window.addEventListener('click', function (ev) {
+	if (!ev.target.matches('.nav__item')) {
+		let dropDowns = document.getElementsByClassName('dropdown');
+		let i;
+		for (i = 0; i < dropDowns.length; i++) {
+			let openSharedown = dropDowns[i];
+			if (openSharedown.classList.contains('show')) {
+				openSharedown.classList.remove('show');
+			}
+		}
+	}
+});
