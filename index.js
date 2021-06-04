09466a14fa272609e3e8551e4e47ed309cb9d9e9
Ikem Nomso <ikemnomso@gmail.com>
2021-06-04 18:52:02 +0100

    style fix

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 function dropDown(str) {
-	document.getElementById(str).classList.toggle('show');
+	document.getElementById(str).classList.toggle('show--dropdown');
+
+	// switch (str) {
+	// 	case 'productDropdown':
+	// 		document.getElementById('productarrow').classList.toggle('rotate--icon');
+	// 		break;
+	// 	case 'companyDropdown':
+	// 		document.getElementById('companyarrow').classList.toggle('rotate--icon');
+	// 		break;
+	// 	case 'connectDropdown':
+	// 		document.getElementById('connectarrow').classList.toggle('rotate--icon');
+	// 		break;
+	// 	default:
+	// 		return;
+	// }
+
+	// document.get;
 }
 
 function toggleNav() {
-	document.getElementById('nav').classList.toggle('show__mobilenav');
+	document.getElementById('nav').classList.toggle('show--mobilenav');
 }
 
 window.addEventListener('click', function (ev) {
@@ -12,8 +28,8 @@ window.addEventListener('click', function (ev) {
 		let i;
 		for (i = 0; i < dropDowns.length; i++) {
 			let openSharedown = dropDowns[i];
-			if (openSharedown.classList.contains('show')) {
-				openSharedown.classList.remove('show');
+			if (openSharedown.classList.contains('show--dropdown')) {
+				openSharedown.classList.remove('show--dropdown');
 			}
 		}
 	}
